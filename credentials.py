f266946ea27001afbb15d5afe0d44ad2ee49b892
Matt Chastain <matthewdchastain@gmail.com>
2017-04-07 21:46:13 -0400

    can add keywords and urls to postgres dynamically; need to address updating rather than dumping pre-existing tables

---
+++ credentials.py
@@ -0,0 +1,5 @@
+DB_URL = "postgres://nqwfqvdlpbqfez:6c4db5c295f36257274449479f0fa45f4b7506cadb403992ec0dbede31cb6f4d@ec2-107-21-109-15.compute-1.amazonaws.com:5432/dcnu3815orbc2h"
+HOSTNAME = "ec2-107-21-109-15.compute-1.amazonaws.com"
+USERNAME = "nqwfqvdlpbqfez"
+PASSWORD = "6c4db5c295f36257274449479f0fa45f4b7506cadb403992ec0dbede31cb6f4d"
+DATABASE = "dcnu3815orbc2h"
