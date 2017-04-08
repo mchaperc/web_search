d40e88968330edb27d51c784ea3b4d09354b8daa
Matt Chastain <matthewdchastain@gmail.com>
2017-04-08 14:24:29 -0400

    cors still

--- endpoints.py
+++ endpoints.py
@@ -8,13 +8,17 @@ from jobs import user_search
 from utils import records_to_json
 
 
+public_cors = CORS(allow_all_origins=True)
+
+
 class SearchResource(object):
-    cors_enabled = False
+    cors = public_cors
     def on_get(self, req, resp, search_query):
         """Handles GET requests"""
         search_results = user_search(search_query)
         resp.status = falcon.HTTP_200
         resp.body = json.dumps(records_to_json(search_results))
+        print(resp.status, resp.body)
 
 
 app = falcon.API()
