b2490a82c089b1e5bbc5127873e0bbfd2f9968a8
Matt Chastain <matthewdchastain@gmail.com>
2017-04-11 18:19:43 -0400

    unbalanced parenthesis

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ from jobs import crawl_web
 from credentials import DB_URL
 from constants import links
 
-schedule.every().weeks(4).do(crawl_web(links)
+schedule.every().weeks(4).do(crawl_web(links))
